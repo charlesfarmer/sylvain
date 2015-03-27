3a698da56115c2e8e65ae93092b978d688027199
charlesfarmer <charles.farmer.1@collegeahuntsic.qc.ca>
2015-03-27 19:03:51 -0400

    Création de la majorité du squelette du Backend Correction du schéma Entity pour que les valeurs Decimal deviennent Decimal(18,6) Extension des classes partielles auto-générées

---
+++ ProjetSylvain/Backend/Partial Classes/ProfesseurHoraire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Backend
+{
+    public partial class ProfesseurHoraire
+    {
+        // définitions additionnelles non détruites
+        // à la regénération du schéma Entity
+    }
+}
---
+++ ProjetSylvain/Backend/Services/Implementations/ServiceJour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Backend.Services.Interfaces;
+
+namespace Backend.Services.Implementations
+{
+    class ServiceJour : Service, IServiceJour
+    {
+    }
+}
